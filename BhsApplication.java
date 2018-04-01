107b21c03e743419407f77d0bee6957efdd165be
maowenjun <913451580@qq.com>
2018-04-01 10:43:18 +0800

    Add files via upload

---
+++ BhsApplication.java
@@ -0,0 +1,12 @@
+package com.bhs.app.bhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhsApplication.class, args);
+	}
+}
